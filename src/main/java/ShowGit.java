edbcbd3d37026b7ce9c502ea674d431fe824bf31
dipti <diptimaddiwar@gmail>
2025-12-24 15:32:38 -0500

    adding second entry

--- src/main/java/ShowGit.java
+++
@@ -1,9 +0,0 @@
-
-public class ShowGit {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-    System.out.println("My code is in git ");
-	}
-
-}
---
+++ src/test/java/SecondEntry.java
@@ -0,0 +1,8 @@
+
+public class SecondEntry {
+
+	public static void main(String[] args) {
+		System.out.println("I am doing second entry to git");
+	}
+
+}
