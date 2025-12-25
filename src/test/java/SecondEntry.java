33a64ad8dba8bdebb16a05ffdff2b53e8d0c82aa
dipti <diptimaddiwar@gmail>
2025-12-25 06:45:21 -0500

    added new line

--- src/test/java/SecondEntry.java
+++ src/test/java/SecondEntry.java
@@ -3,6 +3,7 @@ public class SecondEntry {
 
 	public static void main(String[] args) {
 		System.out.println("I am doing second entry to git");
+		System.out.println("I have entered new info");
 	}
 
 }
